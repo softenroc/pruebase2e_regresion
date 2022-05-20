33ec8ad16dda33ee432b7f8e14930b81ded2ed1f
Dario Fernando Rojas <df.rojasr1@uniandes.edu.co>
2022-05-19 23:34:23 -0500

    Modificaciones en funcionalidad acceso 2 con data pool

--- cypress/cypress/integration/features/ghost3/acceso/funcionalidad_acceso_2.spec.js
+++ cypress/cypress/integration/features/ghost3/acceso/funcionalidad_acceso_2.spec.js
@@ -1,15 +1,27 @@
+const fs = require('fs');
+const  path_data = "data/acceso/";
+
 describe("Feature: Acceso a la plataforma", () => {
   beforeEach(() => {
     cy.home_ghost3();
   });
 
-  it(" Como usuario no registrado ingreso un usuario y una contraseña invalida entonces retorne mensaje indicando que el usuario no existe", () => {
-    cy.log("When I enter email not registry");
-    cy.get('input[id="ember8"]').type("bad@uniandes.edu.co");
-    cy.wait(2000);
-    cy.log("And In ghost3 I enter password  ");
-    cy.get('input[id="ember10"]').type("313213");
-    cy.wait(2000);
+  it(" Como usuario no registrado ingreso un usuario y una contraseña invalida entonces retorne mensaje indicando que el usuario no existe", () => {    
+
+    cy.readFile(path_data+'data_funcionalidad_acceso_2.json').then(data => {   
+      var size = data.length;  
+      var index = Math.floor(Math.random() * size);
+      cy.log("size data pool:"+size);  
+      cy.log("When I enter email not registry: "+data[index].email);  
+      cy.get('input[id="ember8"]').type(data[index].email);
+      cy.wait(2000);
+
+      cy.log("And In ghost3 I enter password  "+data[index].password);
+      cy.get('input[id="ember10"]').type(data[index].password);
+      cy.wait(2000);
+    });
+
+
     cy.log("And I click on element having id ember12");
     cy.get('button[id="ember12"]').click();
     cy.wait(3000);
