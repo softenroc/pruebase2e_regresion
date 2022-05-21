2f53ffebfd0c555a1d367414f718a8796e2c4124
Dario Fernando Rojas <df.rojasr1@uniandes.edu.co>
2022-05-21 12:02:02 -0500

    Actualizar a funciones feature 2 acceso

--- cypress/cypress/integration/features/ghost3/acceso/funcionalidad_acceso_2.spec.js
+++ cypress/cypress/integration/features/ghost3/acceso/funcionalidad_acceso_2.spec.js
@@ -7,7 +7,12 @@ describe("Feature: Acceso a la plataforma", () => {
   });
 
   it(" Como usuario no registrado ingreso un usuario y una contraseña invalida entonces retorne mensaje indicando que el usuario no existe", () => {    
+    when_i_enter_email_not_registry_();
+    and_i_click_on_sign_in_button();
+    then_validate_text_main_error();
+  });
 
+  async function when_i_enter_email_not_registry_(){
     cy.readFile(path_data+'data_funcionalidad_acceso_2.json').then(data => {   
       var size = data.length;  
       var index = Math.floor(Math.random() * size);
@@ -20,16 +25,20 @@ describe("Feature: Acceso a la plataforma", () => {
       cy.get('input[id="ember10"]').type(data[index].password);
       cy.wait(2000);
     });
+  }
 
-
+  async function and_i_click_on_sign_in_button(){
     cy.log("And I click on element having id ember12");
     cy.get('button[id="ember12"]').click();
     cy.wait(3000);
+  };
+
+  async function then_validate_text_main_error(){
     cy.log(" Then Validate text main error to login equals to <There is no user with that email address>");
     cy.get('p[class="main-error"]').should(($p) => {
         const message = $p.text();
         expect(message).contain("There is no user with that email address.");
     });
     cy.wait(2000);
-  });
+  };
 });
