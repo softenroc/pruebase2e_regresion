30bc08c29af3049ac94015b7d96c907b8e78a7b7
Dario Fernando Rojas <df.rojasr1@uniandes.edu.co>
2022-05-21 15:51:22 -0500

    Funcionalidad acceso 3 con api mockaroo

--- cypress/cypress/integration/features/ghost3/acceso/funcionalidad_acceso_3.spec.js
+++ cypress/cypress/integration/features/ghost3/acceso/funcionalidad_acceso_3.spec.js
@@ -4,12 +4,8 @@ describe("Feature: Acceso a la plataforma", () => {
     });
   
     it(" Como usuario no registrado quiero recuperar una contraseña entonces retorne mensaje usuario no existe", () => {
-      cy.log("When I enter email not registry");
-      cy.get('input[id="ember8"]').type("test@uniandes.edu.co");      
-      cy.wait(2000);
-      cy.log("And I click on element having id ember11");
-      cy.get('button[id="ember11"]').click();
-      cy.wait(3000);
+      when_i_enter_email_not_registry_();
+      and_i_click_on_forgot_link();
       cy.log("Validate text main error to login equals to <User not found.>");
       cy.get('p[class="main-error"]').should(($p) => {
           const message = $p.text();
@@ -19,3 +15,26 @@ describe("Feature: Acceso a la plataforma", () => {
     });
   });
   
+  async function when_i_enter_email_not_registry_(){
+
+    cy.request('https://api.mockaroo.com/api/23d901c0?count=10&key=2ffac100').should((response) => {
+      var data = response.body;
+      var size = data.length;  
+      var index = Math.floor(Math.random() * size);
+      cy.log("size data pool:"+size);  
+      cy.log("When I enter email not registry: "+data[index].email);  
+      cy.get('input[id="ember8"]').type(data[index].email);
+      cy.wait(2000);
+  });
+}
+  
+  async function and_i_click_on_forgot_link(){  
+    cy.log("And I click on element having id ember11");
+    cy.get('button[id="ember11"]').click();
+    cy.wait(3000);
+  };
+
+ 
+  
+
+ 
