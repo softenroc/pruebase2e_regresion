2ef1208a0b2291d6b5854e927d3c41b3ac3978d3
Dario Fernando Rojas <df.rojasr1@uniandes.edu.co>
2022-05-21 22:57:47 -0500

    Funcionalidad acceso 9 escenarios actualizados

--- cypress/cypress/integration/features/ghost3/acceso/funcionalidad_acceso_7.spec.js
+++ cypress/cypress/integration/features/ghost3/acceso/funcionalidad_acceso_7.spec.js
@@ -6,19 +6,19 @@ describe("Feature: Acceso a la plataforma", () => {
     cy.home_ghost3();
   });
 
-  it(" Como usuario no registrado ingreso un usuario y una contraseña invalida entonces retorne mensaje indicando que el usuario no existe", () => {    
-    when_i_enter_email_not_registry_();
+  it(" Cuando se ingresa a la plataforma con usuario vacio,  entonces retorne mensaje Please fill out the form to sign in", () => {    
+    when_i_enter_email_empty_and_password_random();
     cy.and_i_click_on_sign_in_button();
-    cy.then_validate_text_main_error("There is no user with that email address");
+    cy.then_validate_text_main_error("Please fill out the form to sign in");
   });
 
-  async function when_i_enter_email_not_registry_(){
+  async function when_i_enter_email_empty_and_password_random(){
     cy.readFile(path_data+'data_funcionalidad_acceso_errado.json').then(data => {   
       var size = data.length;  
       var index = Math.floor(Math.random() * size);
       cy.log("size data pool:"+size);  
-      cy.log("When I enter email not registry: "+data[index].email);  
-      cy.get('input[id="ember8"]').type(data[index].email);
+      cy.log("When I enter email empty");  
+      cy.get('input[id="ember8"]').clear({force:true});
       cy.wait(2000);
 
       cy.log("And In ghost3 I enter password  "+data[index].password);
--- cypress/cypress/integration/features/ghost3/acceso/funcionalidad_acceso_9.spec.js
+++ cypress/cypress/integration/features/ghost3/acceso/funcionalidad_acceso_9.spec.js
@@ -6,22 +6,21 @@ describe("Feature: Acceso a la plataforma", () => {
     cy.home_ghost3();
   });
 
-  it(" Como usuario no registrado ingreso un usuario y una contraseña invalida entonces retorne mensaje indicando que el usuario no existe", () => {    
-    when_i_enter_email_not_registry_();
+  it(" Cuando se ingresa a la plataforma un usuario que no es formato de email,  entonces retorne mensaje Please fill out the form to sign in. .", () => {    
+    when_i_enter_email_with_bad_format_and_password_random();
     cy.and_i_click_on_sign_in_button();
-    cy.then_validate_text_main_error("There is no user with that email address");
+    cy.then_validate_text_main_error("Please fill out the form to sign in");
   });
 
-  async function when_i_enter_email_not_registry_(){
-    cy.readFile(path_data+'data_funcionalidad_acceso_errado.json').then(data => {   
+  async function when_i_enter_email_with_bad_format_and_password_random(){
+    cy.readFile(path_data+'data_funcionalidad_acceso_usuario_mal_formato.json').then(data => {   
       var size = data.length;  
       var index = Math.floor(Math.random() * size);
       cy.log("size data pool:"+size);  
       cy.log("When I enter email not registry: "+data[index].email);  
       cy.get('input[id="ember8"]').type(data[index].email);
-      cy.wait(2000);
-
-      cy.log("And In ghost3 I enter password  "+data[index].password);
+      
+      cy.log("And In ghost3 I enter password random:  "+data[index].password);
       cy.get('input[id="ember10"]').type(data[index].password);
       cy.wait(2000);
     });
